760e0ea456fbf7123cc5f72321d72ef8b09b0f90
Morgan Schweers <cyberfox@jbidwatcher.com>
2007-03-17 07:42:30 +0000

    Add SQL to create the database!
    git-svn-id: svn://svn.jbidwatcher.com/jbidwatcher/trunk@66 b1acfa68-eb39-11db-b167-a3a8cd6b847e

--- src/com/jbidwatcher/util/Database.java
+++ src/com/jbidwatcher/util/Database.java
@@ -77,6 +77,56 @@ public class Database {
     return rval;
   }
 
+  public static boolean dbMake(Database db) {
+    try {
+      /*
+         Creating a statement lets us issue commands against
+         the connection.
+       */
+      Statement s = db.getStatement();
+
+      /*
+       * We create a table, add a few rows, and update one.
+       */
+      s.execute("CREATE TABLE auctions(" +
+          "id INT," +
+          "auction_id VARCHAR(40)," +
+          "title VARCHAR(255)," +
+          "start TIMESTAMP," +
+          "end TIMESTAMP," +
+          "seller VARCHAR(200)," +
+          "high_bidder VARCHAR(200)," +
+          "high_bidder_email VARCHAR(255)," +
+          "quantity INT," +
+          "bid_count INT," +
+          "insurance_optional CHAR," +
+          "fixed_price CHAR," +
+          "no_thumbnail CHAR," +
+          "dutch CHAR," +
+          "reserve CHAR," +
+          "private CHAR," +
+          "reserve_met CHAR," +
+          "has_thumbnail CHAR," +
+          "outbid CHAR," +
+          "paypal CHAR," +
+          "feedback INT," +
+          "feedback_percent DECIMAL(5,2)," +
+          "currency VARCHAR(10)," +
+          "current_bid DECIMAL(10,2)," +
+          "minimum_bid DECIMAL(10,2)," +
+          "shipping DECIMAL(10,2)," +
+          "insurance DECIMAL(10,2)," +
+          "buy_now DECIMAL(10,2)," +
+          "usd_current DECIMAL(10,2)," +
+          "usd_buy_now DECIMAL(10,2))");
+      ErrorManagement.logDebug("Created table auctions");
+    } catch(SQLException se) {
+      return false;
+    }
+    return true;
+  }
+
+
   public static boolean dbTest(Database db) {
     try {
       /*
