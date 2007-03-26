03b59b2cca619d0eac4b14e0fd045db9ff957cf5
Morgan Schweers <cyberfox@jbidwatcher.com>
2007-03-26 22:30:55 +0000

    Added a header comment.
    git-svn-id: svn://svn.jbidwatcher.com/jbidwatcher/trunk@68 b1acfa68-eb39-11db-b167-a3a8cd6b847e

--- src/com/jbidwatcher/auction/server/ebay/ebaySearches.java
+++ src/com/jbidwatcher/auction/server/ebay/ebaySearches.java
@@ -1,5 +1,11 @@
 package com.jbidwatcher.auction.server.ebay;
 
+/*
+ * Copyright (c) 2000-2007, CyberFOX Software, Inc. All Rights Reserved.
+ *
+ * Developed by mrs (Morgan Schweers)
+ */
+
 import com.jbidwatcher.util.html.JHTML;
 import com.jbidwatcher.util.ErrorManagement;
 import com.jbidwatcher.util.Externalized;
