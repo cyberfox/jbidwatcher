4e9b932e3cbf16f243b36ecb8c3b8278f18fb82b
Morgan Schweers <cyberfox@jbidwatcher.com>
2007-11-12 19:06:17 +0000

    Make seller a more standard database object; values are set and retrieved from the backing store, and all the typical database access stuff is present.
    git-svn-id: svn://svn.jbidwatcher.com/jbidwatcher/trunk@229 b1acfa68-eb39-11db-b167-a3a8cd6b847e

--- src/com/jbidwatcher/auction/Seller.java
+++ src/com/jbidwatcher/auction/Seller.java
@@ -1,12 +1,7 @@
 package com.jbidwatcher.auction;
 
-import com.jbidwatcher.util.ErrorManagement;
 import com.jbidwatcher.util.db.AuctionDB;
-import com.jbidwatcher.util.db.DBRecord;
 import com.jbidwatcher.xml.XMLElement;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * User: Morgan
@@ -14,142 +9,88 @@ import java.util.Map;
  * Time: 7:27:30 PM
  * To change this template use File | Settings | File Templates.
  */
-public class Seller {
-  static Map<String, Seller> mSellerMap = new HashMap<String,Seller>();
-  String mSeller;
-  String mPositivePercentage;
-  int mFeedback;
-  private Integer mId;
-  private static AuctionDB sDB = null;
+public class Seller extends ActiveRecord {
+  public Seller() { }
 
-  public Seller() {
-    establishSellerDatabase();
-  }
-
-  private Seller(DBRecord record) {
-    mSeller = record.get("seller");
-    try { mFeedback = Integer.parseInt(record.get("feedback")); } catch(Exception e) { mFeedback = 0; }
-    mPositivePercentage = record.get("feedback_percentage");
-  }
-
-  private static void establishSellerDatabase() {
-    if (sDB == null) {
-      try {
-        sDB = new AuctionDB("sellers");
-      } catch (Exception e) {
-        ErrorManagement.handleException("Can't access the sellers table.", e);
-      }
-    }
-  }
-
-  public static Seller makeSeller(String sellerName) {
-    establishSellerDatabase();
-    Seller existing_seller = mSellerMap.get(sellerName);
-    if(existing_seller == null) {
-      DBRecord existing = sDB.findByColumn("seller", sellerName);
-      Seller rval;
-
-      if (existing == null || existing.isEmpty()) {
-      //  This should look up the seller in the database, so there's only one instance of any given seller.
-        rval = new Seller();
-        rval.setSeller(sellerName);
-      } else {
-        rval = new Seller(existing);
-      }
-
-      mSellerMap.put(sellerName, rval);
-      return rval;
-    }
-
-    return existing_seller;
-  }
-
-  public Integer getId() {
-    if(mId == null) {
-      create();
-    }
-    return mId;
-  }
-
-  private void create() {
-    String id = sDB.storeMap(getMap());
-    if(id != null && id.length() != 0) {
-      mId = Integer.parseInt(id);
-    } else {
-      mId = null;
-    }
-  }
-
-  public DBRecord getMap() {
-    DBRecord values = new DBRecord();
-
-    values.put("seller", mSeller);
-    values.put("feedback", Integer.toString(mFeedback));
-    values.put("feedback_percentage", mPositivePercentage);
-
-    return values;
-  }
-
-  public String getSeller() {
-    return mSeller;
-  }
-
-  public void setSeller(String seller) {
-    mSeller = seller;
-  }
-
-  public String getPositivePercentage() {
-    return mPositivePercentage;
-  }
-
-  public void setPositivePercentage(String positivePercentage) {
-    mPositivePercentage = positivePercentage;
-  }
-
-  public int getFeedback() {
-    return mFeedback;
-  }
-
-  public void setFeedback(int feedback) {
-    mFeedback = feedback;
-  }
+  public String getSeller() { return getString("seller"); }
+  public void setSeller(String name) { setString("seller", name); }
+  public String getPositivePercentage() { return getString("feedback_percentage"); }
+  public void setPositivePercentage(String positivePercentage) { setString("feedback_percentage", positivePercentage); }
+  public int getFeedback() { return getInteger("feedback", 0); }
+  public void setFeedback(int feedback) { setInteger("feedback", feedback); }
 
   public XMLElement toXML() {
     XMLElement xmlResult = new XMLElement("seller");
     XMLElement xseller = new XMLElement("name");
     XMLElement xfeedback = new XMLElement("feedback");
     XMLElement xpercentage = new XMLElement("feedback_percent");
-    xseller.setContents(mSeller);
+    xseller.setContents(getSeller());
     xmlResult.addChild(xseller);
 
-    xfeedback.setContents(Integer.toString(mFeedback));
+    xfeedback.setContents(Integer.toString(getFeedback()));
     xmlResult.addChild(xfeedback);
 
-    xpercentage.setContents(mPositivePercentage);
+    xpercentage.setContents(getPositivePercentage());
     xmlResult.addChild(xpercentage);
 
     return xmlResult;
   }
 
-  public static Seller fromXML(XMLElement curElement) {
-    establishSellerDatabase();
+  public static Seller newFromXML(XMLElement curElement) {
+    String seller = curElement.getChild("seller").getContents();
+    if(seller == null || seller.length() == 0) return null;
+
     try {
       Seller rval = new Seller();
       rval.setSeller(curElement.getChild("seller").getContents());
       rval.setFeedback(Integer.parseInt(curElement.getChild("feedback").getContents()));
       rval.setPositivePercentage(curElement.getChild("feedback_percent").getContents());
 
+      rval.saveDB();
       return rval;
     } catch(Exception e) {
       return null;
     }
   }
 
+  public static Seller makeSeller(String sellerName) {
+    if(sellerName == null) return null;
+
+    Seller existing_seller = findFirstBy("seller", sellerName);
+    if (existing_seller == null) {
+      existing_seller = new Seller();
+      existing_seller.setSeller(sellerName);
+      existing_seller.saveDB();
+    }
+
+    return existing_seller;
+  }
+
   public Seller makeSeller(String sellerName, Seller oldSeller) {
     Seller rval = makeSeller(sellerName);
-    rval.setFeedback(oldSeller.mFeedback);
-    rval.setPositivePercentage(oldSeller.mPositivePercentage);
+    rval.setFeedback(oldSeller.getFeedback());
+    rval.setPositivePercentage(oldSeller.getPositivePercentage());
 
+    rval.saveDB();
     return rval;
+  }
+
+  /*************************/
+  /* Database access stuff */
+  /*************************/
+
+  private static AuctionDB sDB;
+
+  protected static String getTableName() { return "sellers"; }
+
+  protected AuctionDB getDatabase() {
+    if (sDB == null) {
+      sDB = openDB(getTableName());
+    }
+    return sDB;
+  }
+
+  public static Seller findFirstBy(String key, String value) {
+    return (Seller) ActiveRecord.findFirstBy(Seller.class, key, value);
   }
 }
