a42efdc763b8fd533a57bb1d472f7c885b87b1b8
Morgan Schweers <cyberfox@jbidwatcher.com>
2007-12-17 02:49:25 +0000

    Fix JBHelp (specifically the FAQ) for Java Regex'es.
    git-svn-id: svn://svn.jbidwatcher.com/jbidwatcher/trunk@267 b1acfa68-eb39-11db-b167-a3a8cd6b847e

--- src/com/jbidwatcher/ui/JBHelp.java
+++ src/com/jbidwatcher/ui/JBHelp.java
@@ -8,12 +8,9 @@ package com.jbidwatcher.ui;
 import java.io.*;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
-//import com.stevesoft.pat.*;
 import com.jbidwatcher.config.JConfig;
 import com.jbidwatcher.util.http.Http;
 import com.jbidwatcher.Constants;
-
-//import java.util.HashMap;
 
 public class JBHelp {
   private static final String paypalDonate =
@@ -22,7 +19,6 @@ public class JBHelp {
   private static final String c_bl = "<font color=\"blue\">";
   private static final String c_rd = "<font color=\"red\">";
   private static final String c_end= "</font>";
-//  private static final int MAX_VARIABLES = 40;
   private static final String HEAD_LOC = "/help/MyHead.jpg";
 
   private JBHelp() {
@@ -65,62 +61,24 @@ public class JBHelp {
     String headImage = JBHelp.class.getResource(HEAD_LOC).toString();
     String basicLocation = headImage.substring(0, headImage.length() - HEAD_LOC.length());
 
-    return munge.replaceAll("<%que (\\w+)%>", "<%que%><a name=\"Q\\1\">").
-        replaceAll("<%toc%>", toc).
-        replaceAll("<%pname%>", Constants.PROGRAM_NAME).
-        replaceAll("<%ver%>", Constants.PROGRAM_VERS).
-        replaceAll("<%title%>", title).
-        replaceAll("<%donate%>", paypalDonate).
-        replaceAll("<%head%>", headImage).
-        replaceAll("<%jar%>", basicLocation).
-        replaceAll("<%c_gr%>", c_gr).
-        replaceAll("<%c_bl%>", c_bl).
-        replaceAll("<%c_rd%>", c_rd).
-        replaceAll("<%cend%>", c_end).
+    munge = munge.replaceAll("<%que ([0-9]+)%>", "<%que%><a name=\"Q$1\">").
+        replaceAll("<%toc%>", Matcher.quoteReplacement(toc)).
+        replaceAll("<%pname%>", Matcher.quoteReplacement(Constants.PROGRAM_NAME)).
+        replaceAll("<%ver%>", Matcher.quoteReplacement(Constants.PROGRAM_VERS)).
+        replaceAll("<%title%>", Matcher.quoteReplacement(title)).
+        replaceAll("<%donate%>", Matcher.quoteReplacement(paypalDonate)).
+        replaceAll("<%head%>", Matcher.quoteReplacement(headImage)).
+        replaceAll("<%jar%>", Matcher.quoteReplacement(basicLocation)).
+        replaceAll("<%c_gr%>", Matcher.quoteReplacement(c_gr)).
+        replaceAll("<%c_bl%>", Matcher.quoteReplacement(c_bl)).
+        replaceAll("<%c_rd%>", Matcher.quoteReplacement(c_rd)).
+        replaceAll("<%cend%>", Matcher.quoteReplacement(c_end)).
         replaceAll("<%que%>", "<li><B><u>Q.</u> ").
         replaceAll("<%ans%>", "<br><u>A.</u></B> ").
         replaceAll("<%end%>", "<br><a href=\"#top\">Top</a></li>").
-        replaceAll("<%jay%>", JBHelp.class.getResource("/jbidwatch64.jpg").toString()).
-        replaceAll("<%auctions_save%>", JConfig.queryConfiguration("savefile"));
-  }
+        replaceAll("<%jay%>", Matcher.quoteReplacement(JBHelp.class.getResource("/jbidwatch64.jpg").toString())).
+        replaceAll("<%auctions_save%>", Matcher.quoteReplacement(JConfig.queryConfiguration("savefile")));
 
-//  public static String preprocess2(StringBuffer helpBuf, String title) {
-//    String munge = helpBuf.toString();
-//
-//    String toc = genTOC(munge);
-//
-//    Regex r = Regex.perlCode("s/<%que (\\w+)%>/<%que%><a name=\"Q\\1\">/");
-//    munge = r.replaceAll(munge);
-//
-//    HashMap<String, String> vars = new HashMap<String, String>(MAX_VARIABLES);
-//    vars.put("<%toc%>", toc);
-//    vars.put("<%pname%>", Constants.PROGRAM_NAME);
-//    vars.put("<%ver%>", Constants.PROGRAM_VERS);
-//    vars.put("<%title%>", title);
-//    vars.put("<%donate%>", paypalDonate);
-//    String headImage = JBHelp.class.getResource(HEAD_LOC).toString();
-//    vars.put("<%head%>", headImage);
-//    String basicLocation = headImage.substring(0, headImage.length() - HEAD_LOC.length());
-//    vars.put("<%jar%>", basicLocation);
-//    vars.put("<%c_gr%>", c_gr);
-//    vars.put("<%c_bl%>", c_bl);
-//    vars.put("<%c_rd%>", c_rd);
-//    vars.put("<%cend%>", c_end);
-//    vars.put("<%que%>", "<li><B><u>Q.</u> ");
-//    vars.put("<%ans%>", "<br><u>A.</u></B> ");
-//    vars.put("<%end%>", "<br><a href=\"#top\">Top</a></li>");
-//    vars.put("<%jay%>", JBHelp.class.getResource("/jbidwatch64.jpg").toString());
-//    vars.put("<%auctions_save%>", JConfig.queryConfiguration("savefile"));
-//
-//    Variables theReplacements = new Variables(vars);
-//    ReplaceRule.define("var_rule", theReplacements);
-//
-//    r = Regex.perlCode("s/<%\\w+%>/${var_rule}/");
-//
-//    String out = r.replaceAll(munge);
-//    //  Run it a second time, to catch TOC entries.
-//    out = r.replaceAll(out);
-//
-//    return out;
-//  }
+    return munge;
+  }
 }
