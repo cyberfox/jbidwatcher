f926a2efd14caeb75139dcad60584cb2458cd538
Morgan Schweers <cyberfox@jbidwatcher.com>
2009-01-16 12:53:47 +0000

    Update the version number to 2.0beta11 for the multisniping fix.
    git-svn-id: svn://svn.jbidwatcher.com/jbidwatcher/trunk@731 b1acfa68-eb39-11db-b167-a3a8cd6b847e

--- src/com/jbidwatcher/util/Constants.java
+++ src/com/jbidwatcher/util/Constants.java
@@ -14,8 +14,8 @@ public class Constants {
  * in just one place.
  */
   public static final String PROGRAM_NAME = "JBidwatcher";
-  public static final String PROGRAM_VERS = "2.0beta10";
-  public static final String SVN_REVISION = "722";
+  public static final String PROGRAM_VERS = "2.0beta11";
+  public static final String SVN_REVISION = "732";
 /** The clock format to use everywhere, when referring to remote times.
  */
   public static final SimpleDateFormat remoteClockFormat = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss z");
