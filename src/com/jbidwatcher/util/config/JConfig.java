e0fa028b07d1f1c19197aef0a3bd6611acd2c184
Morgan Schweers <cyberfox@gmail.com>
2011-06-09 16:00:59 -0700

    Create a 'new' JConfig which mainly inherits from the old.

---
+++ src/com/jbidwatcher/util/config/JConfig.java
@@ -0,0 +1,11 @@
+package com.jbidwatcher.util.config;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: mrs
+ * Date: 6/9/11
+ * Time: 3:48 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class JConfig extends com.cyberfox.util.config.JConfig {
+}
