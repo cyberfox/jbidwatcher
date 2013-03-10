ae062064765badddd9c2dbf4ed09536bf131baff
Morgan Schweers <cyberfox@gmail.com>
2013-03-10 01:35:18 -0800

    Set up a refactoring, moving the base data methods to a base class.

---
+++ src/com/jbidwatcher/auction/AuctionCore.java
@@ -0,0 +1,134 @@
+package com.jbidwatcher.auction;
+
+import com.jbidwatcher.util.Constants;
+import com.jbidwatcher.util.Currency;
+import com.jbidwatcher.util.db.ActiveRecord;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * User: mrs
+ * Date: 3/9/13
+ * Time: 6:32 PM
+ *
+ * This contains the core auction information.
+ */
+public abstract class AuctionCore extends ActiveRecord {
+  private static Map<String, String> mKeys;
+
+  private static void setupKeys() {
+    mKeys = new HashMap<String, String>();
+    mKeys.put("startDate", "started_at");
+    mKeys.put("start", "started_at");
+    mKeys.put("endDate", "ending_at");
+    mKeys.put("seller", "seller_id");
+    mKeys.put("end", "ending_at");
+    mKeys.put("highbidder", "high_bidder");
+    mKeys.put("highBidder", "high_bidder");
+    mKeys.put("highBidderEmail", "high_bidder_email");
+    mKeys.put("itemLocation", "location");
+    mKeys.put("numBids", "bid_count");
+    mKeys.put("bidcount", "bid_count");
+    mKeys.put("insurance_optional", "optional_insurance");
+    mKeys.put("insuranceOptional", "optional_insurance");
+    mKeys.put("noThumbnail", "no_thumbnail");
+    mKeys.put("reserveMet", "reserve_met");
+    mKeys.put("isReserve", "reserve");
+    mKeys.put("fixed", "fixed_price");
+    mKeys.put("fixedPrice", "fixed_price");
+    mKeys.put("isDutch", "dutch");
+    mKeys.put("hasThumb", "has_thumbnail");
+    mKeys.put("currently", "current_bid");
+    mKeys.put("curBid", "current_bid");
+    mKeys.put("minimum", "minimum_bid");
+    mKeys.put("minBid", "minimum_bid");
+    mKeys.put("usprice", "usd_current");
+    mKeys.put("us_cur", "usd_current");
+    mKeys.put("buy_now_us", "usd_buy_now");
+    mKeys.put("buynow", "buy_now");
+  }
+
+  static {
+    setupKeys();
+  }
+
+  public AuctionCore() {
+    setTranslationTable(mKeys);
+  }
+
+  public String getIdentifier() { return getString("identifier"); }
+
+  public String getTitle() { return getString("title"); }
+
+  public String getHighBidder() { return getString("highBidder"); }
+
+  //  public String getHighBidderEmail() { return getString("highBidderEmail"); }
+  public String getItemLocation() { return getString("itemLocation", ""); }
+
+  public boolean isEnded() { return getBoolean("ended"); }
+
+  public Currency getBestPrice() {
+    Currency currentPrice = getCurBid();
+    if (currentPrice == null || currentPrice.isNull()) {
+      currentPrice = getBuyNow();
+    }
+    return currentPrice;
+  }
+
+  public Currency getCurBid() { return getMonetary("curBid"); }
+
+  public Currency getUSCurBid() {
+    if (getCurBid().getCurrencyType() == Currency.US_DOLLAR) {
+      return getCurBid();
+    }
+    return getMonetary("us_cur", Currency.US_DOLLAR);
+  }
+
+  public Currency getMinBid() { return getMonetary("minBid", getCurBid()); }
+
+  public Currency getShipping() { return getMonetary("shipping"); }
+
+  public Currency getInsurance() { return getMonetary("insurance"); }
+
+  public Currency getBuyNow() { return getMonetary("buy_now"); }
+
+  public int getQuantity() { return getInteger("quantity", 1); }
+
+  public int getNumBidders() { return getInteger("numBids", 0); }
+
+  public int getNumBids() { return getNumBidders(); }
+
+  public Date getStartDate() { return getDate("start"); }
+
+  public Date getEndDate() {
+    Date end = getEnd();
+    if(end == null) end = Constants.FAR_FUTURE;
+    return end;
+  }
+
+  protected boolean isReserve() { return getBoolean("isReserve"); }
+
+  public boolean isPrivate() { return getBoolean("isPrivate"); }
+
+  protected boolean isFixedPrice() { return getBoolean("fixed_price"); }
+
+  boolean isReserveMet() { return getBoolean("reserve_met"); }
+
+  protected boolean hasPaypal() { return getBoolean("paypal"); }
+
+  boolean hasThumb() { return getBoolean("has_thumbnail"); }
+
+  boolean isInsuranceOptional() { return getBoolean("insurance_optional", true); }
+
+  protected boolean hasNoThumbnail() { return getBoolean("noThumbnail"); }
+
+  public Currency getUSCur() { return getMonetary("us_cur", Currency.US_DOLLAR); }
+
+  public Currency getBuyNowUS() { return getMonetary("buy_now_us", Currency.US_DOLLAR); }
+
+  public Date getStart() { return getDate("start"); }
+
+  public Date getEnd() { return getDate("end"); }
+}
--- src/com/jbidwatcher/auction/AuctionInfo.java
+++ src/com/jbidwatcher/auction/AuctionInfo.java
@@ -22,55 +22,15 @@ import com.jbidwatcher.util.xml.XMLElement;
 import java.io.File;
 import java.io.IOException;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.List;
 
 //  TODO -- Interface-ize all the getters, and make AuctionEntry and AuctionInfo both implement it.
-public class AuctionInfo extends ActiveRecord
-{
-  private static Map<String, String> mKeys;
+public class AuctionInfo extends AuctionCore {
   private String mThumbnailPath;
   private String potentialThumbnail = null;
   private Object mServer = null; //  TODO --  This is a hack!
   //  It's so that the AuctionServer that creates this can record
   // 'who it is', so the AuctionEntry will pick it up.
-
-  private static void setupKeys() {
-    mKeys = new HashMap<String, String>();
-    mKeys.put("startDate", "started_at");
-    mKeys.put("start", "started_at");
-    mKeys.put("endDate", "ending_at");
-    mKeys.put("seller", "seller_id");
-    mKeys.put("end", "ending_at");
-    mKeys.put("highbidder", "high_bidder");
-    mKeys.put("highBidder", "high_bidder");
-    mKeys.put("highBidderEmail", "high_bidder_email");
-    mKeys.put("itemLocation", "location");
-    mKeys.put("numBids", "bid_count");
-    mKeys.put("bidcount", "bid_count");
-    mKeys.put("insurance_optional", "optional_insurance");
-    mKeys.put("insuranceOptional", "optional_insurance");
-    mKeys.put("noThumbnail", "no_thumbnail");
-    mKeys.put("reserveMet", "reserve_met");
-    mKeys.put("isReserve", "reserve");
-    mKeys.put("fixed", "fixed_price");
-    mKeys.put("fixedPrice", "fixed_price");
-    mKeys.put("isDutch", "dutch");
-    mKeys.put("hasThumb", "has_thumbnail");
-    mKeys.put("currently", "current_bid");
-    mKeys.put("curBid", "current_bid");
-    mKeys.put("minimum", "minimum_bid");
-    mKeys.put("minBid", "minimum_bid");
-    mKeys.put("usprice", "usd_current");
-    mKeys.put("us_cur", "usd_current");
-    mKeys.put("buy_now_us", "usd_buy_now");
-    mKeys.put("buynow", "buy_now");
-  }
-
-  static {
-    setupKeys();
-  }
 
   protected Seller mSeller;
   protected GZip mLoadedPage = null;
@@ -79,9 +39,7 @@ public class AuctionInfo extends ActiveRecord
    * @brief Empty constructor, for XML parsing.
    *
    */
-  public AuctionInfo() {
-    setTranslationTable(mKeys);
-  }
+  public AuctionInfo() { }
 
   protected String[] infoTags = { "title", "seller", "highbidder", "bidcount", "start", "end",
                                 "currently", "dutch", "reserve", "private", "content",
@@ -375,54 +333,6 @@ public class AuctionInfo extends ActiveRecord
   }
 
   public Object getServer() { return mServer; }
-  public void setServer(Object server) { mServer = server; }
-
-  public String getIdentifier() { return getString("identifier"); }
-  public String getTitle() { return getString("title"); }
-  public String getHighBidder() { return getString("highBidder"); }
-//  public String getHighBidderEmail() { return getString("highBidderEmail"); }
-  public String getItemLocation() { return getString("itemLocation", ""); }
-
-  public Currency getBestPrice() {
-    Currency currentPrice = getCurBid();
-    if (currentPrice == null || currentPrice.isNull()) {
-      currentPrice = getBuyNow();
-    }
-    return currentPrice;
-  }
-
-  public Currency getCurBid() { return getMonetary("curBid"); }
-  public Currency getUSCurBid() {
-    if (getCurBid().getCurrencyType() == Currency.US_DOLLAR) {
-      return getCurBid();
-    }
-    return getMonetary("us_cur", Currency.US_DOLLAR);
-  }
-
-  public Currency getMinBid() { return getMonetary("minBid", getCurBid()); }
-  public Currency getShipping() { return getMonetary("shipping"); }
-  public Currency getInsurance() { return getMonetary("insurance"); }
-  public Currency getBuyNow() { return getMonetary("buy_now"); }
-
-  public int getQuantity() { return getInteger("quantity", 1); }
-  public int getNumBidders() { return getInteger("numBids", 0); }
-  public int getNumBids() { return getNumBidders(); }
-
-  Date getStartDate() { return getDate("start"); }
-  Date getEndDate() {
-    Date end = getDate("end");
-    if(end == null) end = Constants.FAR_FUTURE;
-    return end;
-  }
-
-  protected boolean isReserve() { return getBoolean("isReserve"); }
-  public boolean isPrivate() { return getBoolean("isPrivate"); }
-  protected boolean isFixedPrice() { return getBoolean("fixed_price"); }
-  boolean isReserveMet() { return getBoolean("reserve_met"); }
-  protected boolean hasPaypal() { return getBoolean("paypal"); }
-  boolean hasThumb() { return getBoolean("has_thumbnail"); }
-  boolean isInsuranceOptional() { return getBoolean("insurance_optional", true); }
-  protected boolean hasNoThumbnail() { return getBoolean("noThumbnail"); }
 
   public String getSellerName() {
     refreshSeller();
@@ -446,7 +356,8 @@ public class AuctionInfo extends ActiveRecord
     if (mSeller != null) return mSeller.getPositivePercentage();
     return "n/a";
   }
-  int getFeedbackScore() {
+
+  public int getFeedbackScore() {
     refreshSeller();
     if (mSeller != null) return mSeller.getFeedback();
     return 0;
@@ -467,12 +378,6 @@ public class AuctionInfo extends ActiveRecord
     }
     setInteger("seller_id", seller_id);
   }
-
-  public Currency getUSCur() { return getMonetary("us_cur", Currency.US_DOLLAR); }
-  public Currency getBuyNowUS() { return getMonetary("buy_now_us", Currency.US_DOLLAR); }
-
-  public Date getStart() { return getDate("start"); }
-  public Date getEnd() { return getDate("end"); }
 
   public void setIdentifier(String id) { setString("identifier", id); }
   public void setHighBidder(String highBidder) { setString("highBidder", highBidder); }
