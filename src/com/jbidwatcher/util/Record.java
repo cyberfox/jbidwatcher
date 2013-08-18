c4b007fb972c03ae1c6b67654e4eeae4fedcfe9b
Morgan Schweers <cyberfox@gmail.com>
2013-08-17 17:05:47 -0700

    Add a debugging method to the Record class.

--- src/com/jbidwatcher/util/Record.java
+++ src/com/jbidwatcher/util/Record.java
@@ -9,4 +9,23 @@ import java.util.HashMap;
 * Time: 1:44:08 AM
 * To change this template use File | Settings | File Templates.
 */
-public class Record extends HashMap<String, String> { }
+public class Record extends HashMap<String, String> {
+  public String dump() {
+    StringBuffer sb = dumpRecord(0);
+    sb.append("\n");
+    return sb.toString();
+  }
+
+  private StringBuffer dumpRecord(int offset) {
+    StringBuffer sb = new StringBuffer();
+    sb.append("{");
+    for (Object o : keySet()) {
+      Object value = get(o);
+      if (value != null) {
+        sb.append("\"").append(o.toString()).append("\" => \"").append(value.toString().replace("\"", "\\\"")).append("\"");
+      }
+    }
+    sb.append("}");
+    return sb;
+  }
+}
