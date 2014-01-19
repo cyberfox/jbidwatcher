9fecd736934854edc7359deba12995be7264fc93
Morgan Schweers <cyberfox@gmail.com>
2014-01-18 16:53:30 -0800

    Bump the version number to 2.5.5.

--- src/com/jbidwatcher/util/Constants.java
+++ src/com/jbidwatcher/util/Constants.java
@@ -14,7 +14,7 @@ public class Constants {
  * in just one place.
  */
   public static final String PROGRAM_NAME = "JBidwatcher";
-  public static final String PROGRAM_VERS = "2.5.5pre3";
+  public static final String PROGRAM_VERS = "2.5.5";
   public static String REVISION() {
     String rval = Constants.class.getPackage().getImplementationVersion();
     if(rval == null) return "debug";
